46771ec4668e080dc86cb1ffe9b7c362c2f40cb8
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2020-08-14 00:26:51 -0300

    Coin script creation.

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private Transform backPoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        backPoint = GameObject.Find("Back Point").GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < backPoint.position.x)
+        {
+            Destroy(gameObject);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collider) 
+    {
+        if(collider.gameObject.tag == "Player")
+        {
+            GameController.current.AddCoinScore(1);
+            Destroy(gameObject);
+        }
+    }
+}
