5309f177f7d8f7aa4c93fedabef3ae871b67b9c7
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2020-08-15 11:09:46 -0300

    Enemy spawner script creation.

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public List<GameObject> enemyList = new List<GameObject>();
+    public float initialTime;
+    public float minTime;
+    public float maxTime;
+    private float timeControl;
+    // Start is called before the first frame update
+    void Start()
+    {}
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Spawnar enemys
+        timeControl += Time.deltaTime;
+        if(timeControl >= initialTime)
+        {
+            Instantiate(enemyList[Random.Range(0, enemyList.Count)], 
+            transform.position + new Vector3(0f , Random.Range(-1, 4), 0f) , transform.rotation);
+           initialTime = Random.Range(minTime, maxTime);
+           timeControl = 0;
+        }
+    }
+}
