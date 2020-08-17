b49c9ade8a8b81650646ba93773155dfb7cd98ef
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2020-08-17 01:15:48 -0300

    Uncotrolled enemy script creation.

---
+++ Assets/Scripts/UncontrolledEnemy.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UncontrolledEnemy : MonoBehaviour
+{
+    private Transform backPoint;
+    private Rigidbody2D rigid;
+    private Animator anin;
+    public float speedX;
+    public float speedY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        backPoint = GameObject.Find("Back Point").GetComponent<Transform>();
+        rigid = GetComponent<Rigidbody2D>();
+        anin = GetComponent<Animator>();        
+    }
+    private bool rideUp;
+    // Update is called once per frame
+    void Update()
+    {   
+        if(transform.position.y >= 2f)
+        {
+            rideUp = false;
+        }
+        else if(transform.position.y <= -2f)
+        {
+            rideUp = true;
+        } 
+        if(rideUp)
+        {
+            rigid.velocity = new Vector2(- speedX, speedY);
+        }
+        else
+        {
+            rigid.velocity = new Vector2(- speedX, - speedY);
+        } 
+
+        if(transform.position.x < backPoint.position.x)
+        {
+            Destroy(gameObject);
+        }      
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider) 
+    {
+        if(collider.gameObject.tag == "Bullet")
+        {
+            GetComponent<CircleCollider2D>().enabled = false;
+            anin.SetTrigger("destroy");
+            Destroy(gameObject, 0.6f);
+        }        
+    }
+}
