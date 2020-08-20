dcbec7a59f731c344056c7a989aaca72facddab7
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2020-08-20 01:37:10 -0300

    Platforns upgrade.

--- Assets/Scripts/Platform.cs
+++ Assets/Scripts/Platform.cs
@@ -5,10 +5,17 @@ using UnityEngine;
 public class Platform : MonoBehaviour
 {
     private Transform backPoint;
+    public Transform xObject;
+    public bool isBig;
     // Start is called before the first frame update
     void Start()
     {
         backPoint = GameObject.Find("Back Point").GetComponent<Transform>();
+
+        if(!isBig)
+        {
+            transform.position += new Vector3(0f, -0.8f, 0f);
+        } 
     }
 
     // Update is called once per frame
