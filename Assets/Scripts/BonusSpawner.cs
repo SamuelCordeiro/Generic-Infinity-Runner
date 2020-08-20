b2bcdf5f8c8118bdf4558b3f93ed5270fc18d835
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2020-08-20 01:34:09 -0300

    Bonus Bullets animations creation.

---
+++ Assets/Scripts/BonusSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonusSpawner : MonoBehaviour
+{
+    public GameObject bonus;
+    public float initialTime;
+    public float minTime;
+    public float maxTime;
+    private float timeControl;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(GameController.current.isPlayerAlive)
+        {
+            timeControl += Time.deltaTime;
+            if(timeControl >= initialTime)
+            {
+                Instantiate(bonus, transform.position + new Vector3(0f , Random.Range(-1, 4), 0f) , transform.rotation);
+                initialTime = Random.Range(minTime, maxTime);
+                timeControl = 0;
+            }
+        }
+    }
+}
