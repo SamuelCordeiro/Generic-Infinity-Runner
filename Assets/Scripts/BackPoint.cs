2981071eaf445042560f2d33ac0d8eac9cd9ab9e
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2020-08-22 00:02:00 -0300

    Back point script creation.

---
+++ Assets/Scripts/BackPoint.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackPoint : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) 
+    {
+        if(collision.gameObject.layer == 8)
+        {
+            Destroy(collision.gameObject, 5f);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider) 
+    {
+        if(collider.gameObject.tag == "Enemy" || collider.gameObject.tag == "Coin" || collider.gameObject.tag == "BonusBullets")
+        {
+            Destroy(collider.gameObject);
+        }
+    }
+}
