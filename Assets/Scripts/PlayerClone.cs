9856b9d67533741d03275beebb684dfa9287b41b
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2020-08-27 01:43:09 -0300

    Player clone script creation.

---
+++ Assets/Scripts/PlayerClone.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerClone : MonoBehaviour
+{
+    private bool rideUp;
+    private bool fire;
+    private Rigidbody2D rigid;
+    private Animator anin;
+    private int life;
+    public float speedX;
+    public float speedY;
+    private float playerDistance;
+    private float bulletCooldown;
+    public Transform firePoint;
+    public GameObject smoke;
+    public GameObject bullet;
+    private GameObject player;
+    void Start()
+    {
+        life = 2;
+        fire = false;
+        speedX = 0f;
+        rigid = GetComponent<Rigidbody2D>();
+        anin = GetComponent<Animator>();
+        player = GameObject.FindGameObjectWithTag("Player");        
+    }
+
+    void Update()
+    {
+        StartMovement();
+        Movement();
+        Fire();     
+    }
+
+    private void StartMovement()
+    {
+        if(player != null)
+        {
+            playerDistance = player.transform.position.x;
+            if(transform.position.x - playerDistance <= 15.5f)
+            {
+                speedX = 6.2f;
+            }
+        }
+    }
+    private void Movement()
+    {
+        if(transform.position.y >= 2f)
+        {
+            rideUp = false;
+        }
+        else if(transform.position.y <= -2f)
+        {
+            rideUp = true;
+        } 
+        if(rideUp)
+        {
+            rigid.velocity = new Vector2(speedX, speedY);
+        }
+        else
+        {
+            rigid.velocity = new Vector2(speedX, - speedY);
+        }
+    }
+    private void Fire()
+    {
+        bulletCooldown += Time.deltaTime;
+        if(fire && bulletCooldown > 2f)
+        {
+            Instantiate(bullet, firePoint.transform.position, firePoint.transform.rotation);
+            bulletCooldown = 0f;
+        } 
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) 
+    {
+        if(collision.gameObject.tag == "Bullet")
+        {
+            if(life > 0)
+            {
+                life--;
+            }
+            else
+            {
+                GetComponent<CapsuleCollider2D>().enabled = false;
+                smoke.SetActive(false);
+                anin.SetTrigger("destroy");
+                Destroy(collision.gameObject);
+                Destroy(gameObject, 0.6f);
+            }
+        } 
+        if(collision.gameObject.tag == "Player")
+        {
+            GetComponent<CapsuleCollider2D>().enabled = false;
+            anin.SetTrigger("destroy");
+            Destroy(collision.gameObject);
+            Destroy(gameObject, 0.6f);
+            GameController.current.GameOver();
+        }
+        // if(collision.gameObject.layer == 8)
+        // {
+        //     rideUp = true;
+        //     //rigid.velocity = new Vector2(rigid.velocity.x , rigid.velocity.y + 50f);
+        // }        
+    }
+    private void OnCollisionStay2D(Collision2D collision) 
+    {
+        if(collision.gameObject.layer == 8)
+        {
+            rideUp = true;
+            rigid.velocity = new Vector2(rigid.velocity.x , rigid.velocity.y * 1000f);
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collider) 
+    {
+        if(collider.gameObject.tag == "Player")
+        {
+            fire = true;
+        }        
+    }
+
+    private void OnTriggerExit2D(Collider2D collider) 
+    {
+        if(collider.gameObject.tag == "Player")
+        {
+            fire = false;
+        }
+        
+    }
+}
