88e7adc04eac059c5f445efb1f8fed0052c125b3
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2020-08-29 18:18:36 -0300

    Player and enemy bullet configurations.

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -5,13 +5,12 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public float speed;
-    // Start is called before the first frame update
+    
     void Start()
     {
        Destroy(gameObject, 1.2f); 
     }
 
-    // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector2.right * speed * Time.deltaTime);
