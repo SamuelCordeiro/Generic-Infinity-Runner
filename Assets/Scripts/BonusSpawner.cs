716c91bb44cfb15abc450f0b0705e9cc19f29889
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2020-08-31 23:16:31 -0300

    Corrected bonus spawner.

--- Assets/Scripts/BonusSpawner.cs
+++ Assets/Scripts/BonusSpawner.cs
@@ -9,13 +9,7 @@ public class BonusSpawner : MonoBehaviour
     public float minTime;
     public float maxTime;
     private float timeControl;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
         if(GameController.current.isPlayerAlive)
@@ -23,7 +17,7 @@ public class BonusSpawner : MonoBehaviour
             timeControl += Time.deltaTime;
             if(timeControl >= initialTime)
             {
-                Instantiate(bonus, transform.position + new Vector3(0f , Random.Range(-1, 4), 0f) , transform.rotation);
+                Instantiate(bonus, transform.position + new Vector3(0f , Random.Range(1, 4), 0f) , transform.rotation);
                 initialTime = Random.Range(minTime, maxTime);
                 timeControl = 0;
             }
