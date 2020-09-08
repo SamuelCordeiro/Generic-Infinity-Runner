0d0df699468d6f747671464a4b36146e7796fb66
Samuel Cordeiro Silva Oliveira <40183730+SamuelCordeiro@users.noreply.github.com>
2020-09-07 23:51:45 -0300

    Correction in menu screens.

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
@@ -11,20 +12,35 @@ public class GameController : MonoBehaviour
     public float scoreUpdated;
     public Text coinText;
     public int coinScore;
+    public Text totalBulletText;
+    public int totalBullet;
+    public GameObject gameOver;
+    public Text gameOverScore;
+    public Text gameOverCoinScore;
+    public bool isPlayerAlive;
     public static GameController current;
-    // Start is called before the first frame update
+
     void Start()
     {
         current = this;
+        totalBullet = 10;
+        isPlayerAlive = true;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        scoreUpdated += scorePerSecond * Time.deltaTime;
-        score = (int)scoreUpdated;
-        scoreText.text = score.ToString();
-        coinText.text = coinScore.ToString();
+        ScoreUpgrade();  
+    }
+    private void ScoreUpgrade()
+    {
+        if(isPlayerAlive)
+        {
+            scoreUpdated += scorePerSecond * Time.deltaTime;
+            score = (int)scoreUpdated;
+            scoreText.text = score.ToString() + " m";
+            coinText.text = coinScore.ToString();
+            totalBulletText.text = totalBullet.ToString();
+        }
     }
 
     public void AddScore(float value)
@@ -35,5 +51,27 @@ public class GameController : MonoBehaviour
     public void AddCoinScore(int value)
     {
         coinScore += value;
+    }
+
+    public void AddBullets(int value)
+    {
+        totalBullet += value;
+    }
+
+    public void SubBullets()
+    {
+        totalBullet --;
+    }
+    public void GameOver()
+    {
+        isPlayerAlive = false;
+        gameOver.SetActive(true);
+        gameOverScore.text = score.ToString() + " m";
+        gameOverCoinScore.text = "x " + coinScore.ToString();
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene(0);
     }
 }
